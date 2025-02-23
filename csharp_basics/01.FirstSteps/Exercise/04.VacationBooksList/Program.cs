4e8684ecaeb24d05dd76236cb11b161de26902b1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 10:49:56 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/04.VacationBooksList/Program.cs
@@ -0,0 +1,5 @@
+﻿int pages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+Console.WriteLine(pages / pagesPerHour / days);
