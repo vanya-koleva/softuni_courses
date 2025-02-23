3850d1de07aaf656707b78ed293a3007b8160360
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 11:27:26 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,14 @@
+﻿int countChickenMenu = int.Parse(Console.ReadLine());
+int countFishMenu = int.Parse(Console.ReadLine());
+int countVegetarianMenu = int.Parse(Console.ReadLine());
+
+double priceChickenMenu = 10.35;
+double priceFishMenu = 12.4;
+double priceVegetarianMenu = 8.15;
+double priceDelivery = 2.50;
+
+double price = countChickenMenu * priceChickenMenu + countFishMenu * priceFishMenu + countVegetarianMenu * priceVegetarianMenu;
+double priceDessert = price * 0.2;
+double totalPrice = price + priceDessert + priceDelivery;
+
+Console.WriteLine(totalPrice);
