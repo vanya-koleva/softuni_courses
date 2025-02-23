6eddef77de2b4c1540118cb5b4606b78b22dd1be
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 10:48:57 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/03.DepositCalculator/Program.cs
@@ -0,0 +1,5 @@
+﻿double depositedSum = double.Parse(Console.ReadLine());
+int monthsOfDeposit = int.Parse(Console.ReadLine());
+double yearlyPercent = double.Parse(Console.ReadLine());
+
+Console.WriteLine(depositedSum + monthsOfDeposit * (depositedSum * yearlyPercent / 100 / 12));
