83841029dde4d2b05414ec301661d83d118d3c36
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 10:50:16 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿int pens = int.Parse(Console.ReadLine());
+int markers = int.Parse(Console.ReadLine());
+int detergent = int.Parse(Console.ReadLine());
+int discountPercentage = int.Parse(Console.ReadLine());
+
+double pricePerPen = 5.8;
+double pricePerMarker = 7.2;
+double pricePerDetergent = 1.2; 
+
+double totalSum = pens * pricePerPen + markers * pricePerMarker + detergent * pricePerDetergent;
+
+Console.WriteLine(totalSum - (totalSum * discountPercentage / 100));
