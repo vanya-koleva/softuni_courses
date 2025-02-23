3bf7e419eea0f1a15da849ffa5c8859cc1c09ed6
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 13:20:31 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/09.FishTank/Program.cs
@@ -0,0 +1,10 @@
+﻿int volume = int.Parse(Console.ReadLine())
+            * int.Parse(Console.ReadLine())
+            * int.Parse(Console.ReadLine());
+
+double percentage = double.Parse(Console.ReadLine()) * 0.01;
+
+double volumeInLiters = volume * 0.001;
+double freeSpace = volumeInLiters * (1 - percentage);
+
+Console.WriteLine(freeSpace);
