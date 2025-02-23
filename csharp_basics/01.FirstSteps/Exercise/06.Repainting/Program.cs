107d7c33691a14bfe785dee8b470d608499ef736
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 10:50:36 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/06.Repainting/Program.cs
@@ -0,0 +1,17 @@
+﻿int nylon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int thinner = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+double pricePerNylon = 1.5;
+double pricePerPaint = 14.5;
+double pricePerThinner = 5;
+double priceOfBags = 0.4;
+
+double totalNylon = (nylon + 2) * pricePerNylon;
+double totalPaint = (paint * 1.1) * pricePerPaint;
+double totalMaterialsPrice = totalNylon + totalPaint + thinner * pricePerThinner + priceOfBags;
+double workersPrice = (totalMaterialsPrice * 0.3) * hours;
+
+double totalPrice = totalMaterialsPrice + workersPrice;
+Console.WriteLine(totalPrice);
