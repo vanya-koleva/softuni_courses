69c7c67ec73b3fc4732012282d05b47efad193ef
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-02-23 13:20:17 +0200

    Add a solution

---
+++ csharp_basics/01.FirstSteps/Exercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,9 @@
+﻿int yearlyFee = int.Parse(Console.ReadLine());
+
+double sneakersPrice = yearlyFee * 0.6;
+double outfitPrice = sneakersPrice * 0.8;
+double ballPrice = outfitPrice * 0.25;
+double accessoriesPrice = ballPrice * 0.2;
+double totalPrice = yearlyFee + sneakersPrice + outfitPrice + ballPrice + accessoriesPrice;
+
+Console.WriteLine(totalPrice);
