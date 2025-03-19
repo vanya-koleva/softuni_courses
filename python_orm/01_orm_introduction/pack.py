2384dfc1c75e181bbaf6eabbb35cb4b2d62e7721
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-03-19 02:21:22 +0200

    fix: exclude .venv from zip

--- python_orm/01_orm_introduction/pack.py
+++ python_orm/01_orm_introduction/pack.py
@@ -17,7 +17,8 @@ def pack():
                 current_dir = os.path.basename(root)
                 if file in ['requirements.txt', 'manage.py', 'caller.py']\
                         or current_dir in ['main_app', 'orm_skeleton', 'migrations']:
-                    zipf.write(file_path, archive_path)
+                    if '.venv' not in file_path:
+                        zipf.write(file_path, archive_path)
 
     print('Submission created!')
 
