fedb1692c5eb104c52589cb499f06390ad565d98
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-03-24 19:12:24 +0200

    Add a solution

--- python_orm/11_exam_prep/04_exam_11_december_2023/caller.py
+++ python_orm/11_exam_prep/04_exam_11_december_2023/caller.py
@@ -7,7 +7,7 @@ django.setup()
 
 from django.db.models import Q, Count
 
-from main_app.models import TennisPlayer
+from main_app.models import TennisPlayer, Tournament, Match
 
 
 def get_tennis_players(search_name=None, search_country=None) -> str:
@@ -57,3 +57,69 @@ def get_tennis_player_by_matches_count() -> str:
 
     return (f"Tennis Player: {player.full_name} "
             f"with {player.num_of_matches} matches played.")
+
+def get_tournaments_by_surface_type(surface=None) -> str:
+    if surface is None:
+        return ""
+
+    tournaments = (Tournament.objects.prefetch_related(
+        'matches'
+    ).annotate(
+        num_matches=Count('matches')
+    ).filter(
+        surface_type__icontains=surface
+    ).order_by(
+        '-start_date'
+    ))
+
+    if not tournaments:
+        return ""
+
+    return "\n".join(f"Tournament: {t.name}, "
+                     f"start date: {t.start_date}, "
+                     f"matches: {t.num_matches}"
+                     for t in tournaments)
+
+def get_latest_match_info() -> str:
+    match = Match.objects.prefetch_related(
+        'players'
+    ).order_by(
+        'date_played', 'id'
+    ).last()
+
+    if match is None:
+        return ""
+
+    players = match.players.order_by('full_name')
+    player1_full_name = players.first().full_name
+    player2_full_name = players.last().full_name
+    winner_full_name = match.winner.full_name if match.winner else "TBA"
+
+    return (f"Latest match played on: {match.date_played}, "
+            f"tournament: {match.tournament.name}, "
+            f"score: {match.score}, "
+            f"players: {player1_full_name} vs {player2_full_name}, "
+            f"winner: {winner_full_name}, "
+            f"summary: {match.summary}")
+
+def get_matches_by_tournament(tournament_name=None) -> str:
+    if tournament_name is None:
+        return "No matches found."
+
+    matches = Match.objects.select_related(
+        'tournament', 'winner'
+    ).filter(
+        tournament__name=tournament_name
+    ).order_by(
+        '-date_played'
+    )
+
+    if not matches:
+        return "No matches found."
+
+    return "\n".join(
+        f"Match played on: {m.date_played}, "
+        f"score: {m.score}, "
+        f"winner: {m.winner.full_name if m.winner else 'TBA'}"
+        for m in matches
+    )
