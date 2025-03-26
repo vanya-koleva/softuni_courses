7efcfbd988e5a23f9df84b89342eadc8e7e4fb80
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-03-26 14:42:26 +0200

    Add a solution

--- python_orm/11_exam_prep/05_exam_03_august_2024/caller.py
+++ python_orm/11_exam_prep/05_exam_03_august_2024/caller.py
@@ -5,6 +5,50 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Astronaut
+from django.db.models import Q, Count
 
-# Create queries within functions
+
+def get_astronauts(search_string=None) -> str:
+    if search_string is None:
+        return ""
+
+    astronauts = Astronaut.objects.filter(
+        Q(name__icontains=search_string)
+            |
+        Q(phone_number__icontains=search_string)
+    ).order_by(
+        'name'
+    )
+
+    if len(astronauts) == 0:
+        return ""
+
+    return "\n".join(
+        f"Astronaut: {a.name}, "
+        f"phone number: {a.phone_number}, "
+        f"status: {'Active' if a.is_active else 'Inactive'}"
+        for a in astronauts
+    )
+
+def get_top_astronaut() -> str:
+    astronaut = Astronaut.objects.get_astronauts_by_missions_count().first()
+
+    if astronaut is None or astronaut.missions_count == 0:
+        return "No data."
+
+    return f"Top Astronaut: {astronaut.name} with {astronaut.missions_count} missions."
+
+def get_top_commander() -> str:
+    commander = Astronaut.objects.annotate(
+        commanded_missions_count=Count('commanded_missions')
+    ).order_by(
+        '-commanded_missions_count',
+        'phone_number',
+    ).first()
+
+    if commander is None or commander.commanded_missions_count == 0:
+        return "No data."
+
+    return (f"Top Commander: {commander.name} with "
+            f"{commander.commanded_missions_count} commanded missions.")
