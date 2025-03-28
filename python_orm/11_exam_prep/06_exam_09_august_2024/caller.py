f7a914449f4390536492cbde8b622332439dd037
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-03-28 17:25:12 +0200

    Add a solution

--- python_orm/11_exam_prep/06_exam_09_august_2024/caller.py
+++ python_orm/11_exam_prep/06_exam_09_august_2024/caller.py
@@ -5,8 +5,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from django.db.models import Q, Count
-from main_app.models import House, Dragon
+from django.db.models import Q, Count, F, Min, Avg
+from main_app.models import House, Dragon, Quest
 
 
 def get_houses(search_string=None) -> str:
@@ -55,3 +55,63 @@ def get_most_powerful_dragon() -> str:
             f"with a power level of {dragon.power:.1f}, breath type {dragon.breath}, "
             f"and {dragon.wins} wins, coming from the house of {dragon.house.name}. "
             f"Currently participating in {num_quests} quests.")
+
+def update_dragons_data() -> str:
+    num_of_dragons_affected = Dragon.objects.filter(
+        is_healthy=False,
+        power__gt=Dragon.MIN_POWER,
+    ).update(
+        power=F("power") - 0.1,
+        is_healthy=True,
+    )
+
+    if num_of_dragons_affected == 0:
+        return "No changes in dragons data."
+
+    min_power = Dragon.objects.aggregate(Min("power"))["power__min"]
+
+    return (f"The data for {num_of_dragons_affected} dragon/s has been changed. "
+            f"The minimum power level among all dragons is {min_power:.1f}")
+
+def get_earliest_quest() -> str:
+    quest = Quest.objects.order_by("start_time").first()
+
+    if not quest:
+        return "No relevant data."
+
+    day = quest.start_time.day
+    month = quest.start_time.month
+    year = quest.start_time.year
+
+    dragons = quest.dragons.order_by("-power", "name")
+    dragons_names = "*".join(d.name for d in dragons)
+    avg_power_level = dragons.aggregate(Avg("power"))["power__avg"] or 0
+
+    return (f"The earliest quest is: {quest.name}, "
+            f"code: {quest.code}, start date: {day}.{month}.{year}, "
+            f"host: {quest.host.name}. Dragons: {dragons_names}. "
+            f"Average dragons power level: {avg_power_level:.2f}")
+
+def announce_quest_winner(quest_code) -> str:
+    quest = Quest.objects.filter(code__exact=quest_code).first()
+
+    if not quest:
+        return "No such quest."
+
+    winning_dragon = quest.dragons.order_by("-power", "name").first()\
+
+    winning_dragon.wins += 1
+    winning_dragon.save()
+
+    winning_dragon.house.wins += 1
+    winning_dragon.house.save()
+
+    quest.delete()
+
+    return (f"The quest: {quest.name} "
+            f"has been won by dragon {winning_dragon.name} "
+            f"from house {winning_dragon.house.name}. "
+            f"The number of wins has been updated as follows: "
+            f"{winning_dragon.wins} total wins for the dragon and "
+            f"{winning_dragon.house.wins} total wins for the house. "
+            f"The house was awarded with {quest.reward:.2f} coins.")
