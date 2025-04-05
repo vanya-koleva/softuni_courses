65f7b742ac59e88150f5cf2167c9ed075b13ee6b
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-05 08:00:49 +0300

    Add urls

--- python_orm/12_workshop/fruitipediaApp/fruitipediaApp/fruits/urls.py
+++ python_orm/12_workshop/fruitipediaApp/fruitipediaApp/fruits/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import test
+from .views import index_view, dashboard_view, create_fruit_view, \
+    fruit_details_view, edit_fruit_view, delete_fruit_view, create_category_view
 
 urlpatterns = [
-    path('test/', test),
+    path("", index_view, name="index"),
+    path("dashboard/", dashboard_view, name="dashboard"),
+    path("create-fruit/", create_fruit_view, name="create_fruit"),
+    path("<int:pk>/", include([
+        path("fruit-detail/", fruit_details_view, name="fruit_detail"),
+        path("edit-fruit/", edit_fruit_view, name="edit_fruit"),
+        path("delete-fruit/", delete_fruit_view, name="delete_fruit"),
+    ])),
+    path("create-category/", create_category_view, name="create_category"),
 ]
--- python_orm/12_workshop/fruitipediaApp/fruitipediaApp/fruits/views.py
+++ python_orm/12_workshop/fruitipediaApp/fruitipediaApp/fruits/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def test(request):
-    return HttpResponse("Hello, new app!")
+
+def index_view(request):
+    return render(request, "common/index.html")
+
+
+def dashboard_view(request):
+    return render(request, "common/dashboard.html")
+
+
+def create_fruit_view(request):
+    return render(request, "fruits/create-fruit.html")
+
+
+def fruit_details_view(request, pk):
+    return render(request, "fruits/details-fruit.html")
+
+
+def edit_fruit_view(request, pk):
+    return render(request, "fruits/edit-fruit.html")
+
+
+def delete_fruit_view(request, pk):
+    return render(request, "fruits/delete-fruit.html")
+
+
+def create_category_view(request):
+    return render(request, "categories/create-category.html")
