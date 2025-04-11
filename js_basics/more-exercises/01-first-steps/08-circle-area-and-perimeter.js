a58e508d00564f79a22e4b4d05c2a4fdc40c9779
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-11 15:27:24 +0300

    Add a solution

---
+++ js_basics/more-exercises/01-first-steps/08-circle-area-and-perimeter.js
@@ -0,0 +1,10 @@
+function solve(r) {
+    const area = Math.PI * r ** 2;
+    const perimeter = 2 * Math.PI * r;
+
+    console.log(area.toFixed(2));
+    console.log(perimeter.toFixed(2));
+}
+
+// solve(3);
+
