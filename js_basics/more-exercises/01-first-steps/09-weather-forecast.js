1df92e3ade779e3257b35044be6f8b103125dbd1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-11 15:27:41 +0300

    Add a solution

---
+++ js_basics/more-exercises/01-first-steps/09-weather-forecast.js
@@ -0,0 +1,10 @@
+function solve(weather) {
+    if (weather === 'sunny') {
+        console.log("It's warm outside!");
+    } else {
+        console.log("It's cold outside!");
+    }
+}
+
+// solve('sunny');
+
