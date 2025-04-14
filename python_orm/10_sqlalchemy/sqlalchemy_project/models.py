1ce714927ea3541b8a9299f695cb97e97a41b02d
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-14 12:34:32 +0300

    Add a solution

--- python_orm/10_sqlalchemy/sqlalchemy_project/models.py
+++ python_orm/10_sqlalchemy/sqlalchemy_project/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
 '''
 <db_type>+<db_driver>://<user_name>:<password>@<host>:<port>/<db_name>
@@ -6,3 +7,20 @@ from sqlalchemy import create_engine
 CONNECTION_STRING = 'postgresql+psycopg2://postgres-user:password@localhost:5432/sqlalchemy_test'
 
 engine = create_engine(CONNECTION_STRING)
+Base = declarative_base()
+
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(30), nullable=False, default='Pencho')
+    last_name = Column(String(30), nullable=False, default='Nechitankov')
+    age = Column(Integer)
+    salary = Column(Integer)
+
+Base.metadata.create_all(engine)
+
+
+
+
+
