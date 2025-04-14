697265890fe1180e9fa5b1c54abf7266d52709a7
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-14 16:11:11 +0300

    Add a solution

--- python_orm/10_sqlalchemy/sqlalchemy_project/main.py
+++ python_orm/10_sqlalchemy/sqlalchemy_project/main.py
@@ -1,6 +1,8 @@
+import random
+
 from sqlalchemy import or_
 
-from models import engine, Employee
+from models import engine, Employee, City
 
 from sqlalchemy.orm import sessionmaker
 
@@ -9,7 +11,7 @@ Session = sessionmaker(bind=engine)
 # session = Session()   # Not recommended
 # session.close()
 
-with Session() as session:
+# with Session() as session:
     # # filter_by - only with =
     # employees = session.query(Employee).filter_by(age=20)
     # employees = session.query(Employee).filter(Employee.age > 40)
@@ -29,15 +31,62 @@ with Session() as session:
     # employee = session.query(Employee).order_by(Employee.id.desc()).first()
     # print(employee.first_name, employee.last_name)
 
-    # Update
-    employee = session.query(Employee).filter_by(id=1).first()
-    print(employee.first_name, employee.age)
-    employee.age = 70
-    session.commit()
-    employee = session.query(Employee).filter_by(id=1).first()
-    print(employee.first_name, employee.age)
+    # # Update
+    # employee = session.query(Employee).filter_by(id=1).first()
+    # print(employee.first_name, employee.age)
+    # employee.age = 70
+    # session.commit()
+    # employee = session.query(Employee).filter_by(id=1).first()
+    # print(employee.first_name, employee.age)
+    #
+    # # Delete
+    # employee = session.query(Employee).filter_by(id=1).first()
+    # session.delete(employee)
+    # session.commit()
 
-    # Delete
-    employee = session.query(Employee).filter_by(id=1).first()
-    session.delete(employee)
-    session.commit()
+# with Session() as session:
+#     session.add_all((
+#         City(name='Varna'),
+#         City(name='Burgas'),
+#         City(name='Primorsko'),
+#         City(name='Sozopol'),
+#         City(name='Tzarevo'),
+#     ))
+#     session.commit()
+
+# with Session() as session:
+#     employees = session.query(Employee).all()
+#     for e in employees:
+#     #     e.city_id = random.randint(1, 5)
+#     # session.commit()
+#         print(e.first_name, e.last_name, e.city.name)
+
+# with Session() as session:
+#     employees = session.query(Employee).filter(Employee.city_id==1).all()
+#     for e in employees:
+#         print(e.first_name, e.last_name, e.city.name)
+
+# with Session() as session:
+#     employees = session.query(Employee).filter(Employee.city.has(name='Sozopol')).all()
+#     for e in employees:
+#         print(e.first_name, e.last_name, e.city.name)
+
+with Session() as session:
+    # employees = session.query(Employee).filter(Employee.city.has(name='Sozopol')).all()
+
+    # employees = session.query(Employee).join(City).filter(City.name=='Tzarevo')
+    # for e in employees:
+    #     print(e.first_name, e.last_name, e.city.name)
+
+    # city = session.query(City).filter(City.name=='Burgas').first()
+    # for e in city.employees:
+    #     print(e.first_name, e.last_name, city.name)
+
+    # city = session.query(City).order_by(City.name).all()
+    # for c in city:
+    #     for e in c.employees:
+    #         print(e.first_name, e.last_name, c.name)
+
+    employee = session.query(Employee).filter(Employee.id == 2).first()
+    city = session.query(City).filter_by(id=employee.city_id).first()
+    print(employee.first_name, employee.last_name, city.name)
--- python_orm/10_sqlalchemy/sqlalchemy_project/models.py
+++ python_orm/10_sqlalchemy/sqlalchemy_project/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, Relationship
 
 '''
 <db_type>+<db_driver>://<user_name>:<password>@<host>:<port>/<db_name>
@@ -17,10 +17,12 @@ class Employee(Base):
     last_name = Column(String(30), nullable=False, default='Nechitankov')
     age = Column(Integer)
     salary = Column(Integer)
+    city_id = Column(Integer, ForeignKey('cities.id'), default=1)
+    city = Relationship('City', back_populates='employees')
 
 
-
-
-
-
-
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    employees = Relationship('Employee', back_populates='city')
