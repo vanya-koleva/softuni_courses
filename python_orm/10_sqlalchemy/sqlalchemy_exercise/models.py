cd53b86ef49809485ba161b5e35a1e40124b5413
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-16 15:37:10 +0300

    Add a solution

--- python_orm/10_sqlalchemy/sqlalchemy_exercise/models.py
+++ python_orm/10_sqlalchemy/sqlalchemy_exercise/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()   # same as models.Model
 
@@ -24,4 +24,33 @@ class Recipe(Base):
     instructions = Column(
         Text,
         nullable=False,
+    )
+
+    chef = relationship(
+        'Chef',
+        back_populates='recipes',
+    )
+
+
+class Chef(Base):
+    __tablename__ = 'chefs'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    name = Column(
+        String,
+        nullable=False,
+    )
+
+    chef_id = Column(
+        Integer,
+        ForeignKey('chefs.id'),
+    )
+
+    recipes = relationship(
+        'Recipe',
+        back_populates='chef',
     )
