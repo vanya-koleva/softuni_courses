a399f2706e10037e6add97ab490de9124544c1df
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-17 15:09:20 +0300

    Create models for album and song

--- python_orm/13_workshop/musicApp/alembic/env.py
+++ python_orm/13_workshop/musicApp/alembic/env.py
@@ -2,10 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
-
 from musicApp.settings import BaseModel
+from musics.models import Album, Song
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- python_orm/13_workshop/musicApp/musics/models.py
+++ python_orm/13_workshop/musicApp/musics/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from musicApp.settings import BaseModel
 
-# Create your models here.
+
+class Album(BaseModel):
+    __tablename__ = "albums"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    album_name = Column(
+        String(30),
+        nullable=False,
+        unique=True,
+    )
+
+    image_url = Column(
+        String(200),
+        nullable=False,
+    )
+
+    price = Column(
+        Float,
+        nullable=False,
+    )
+
+    songs = relationship(
+        "Song",
+        back_populates="album",
+        cascade="all, delete-orphan",
+    )
+
+
+class Song(BaseModel):
+    __tablename__ = "songs"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    song_name = Column(
+        String(200),
+    )
+
+    album_id = Column(
+        Integer,
+        ForeignKey("albums.id"),
+        nullable=False,
+    )
+
+    album = relationship(
+        "Album",
+        back_populates="songs",
+    )
