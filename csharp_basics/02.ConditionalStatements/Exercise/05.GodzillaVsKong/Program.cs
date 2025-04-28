16a0459e0fce711bcb4584ab27d4db04ff6a7e59
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-29 00:14:07 +0300

    Add a solution

---
+++ csharp_basics/02.ConditionalStatements/Exercise/05.GodzillaVsKong/Program.cs
@@ -0,0 +1,18 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int statistsNum = int.Parse(Console.ReadLine());
+double clothingPrice = double.Parse(Console.ReadLine());
+
+double totalClothing = statistsNum * clothingPrice * (statistsNum > 150 ? 0.9 : 1.0);
+double totalCost = budget * 0.1 + totalClothing;
+double difference = Math.Abs(budget - totalCost);
+
+if (budget >= totalCost)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {difference:F2} leva left.");
+}
+else
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {difference:F2} leva more.");
+}
