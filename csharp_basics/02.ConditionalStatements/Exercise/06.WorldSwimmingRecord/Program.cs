e77487b39e28b52660f94003fc04f50affa279a7
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-29 00:15:03 +0300

    Add a solution

---
+++ csharp_basics/02.ConditionalStatements/Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,19 @@
+﻿double currentRecord = double.Parse(Console.ReadLine());
+double distance = double.Parse(Console.ReadLine());
+double timePerMeter = double.Parse(Console.ReadLine());
+
+const double resistance = 12.5;
+
+double delay = Math.Floor(distance / 15) * resistance;
+
+double timeNeeded = distance * timePerMeter + delay;
+
+if (timeNeeded < currentRecord)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {timeNeeded:f2} seconds.");
+}
+else
+{
+    double difference = timeNeeded - currentRecord;
+    Console.WriteLine($"No, he failed! He was {difference:f2} seconds slower.");
+}
