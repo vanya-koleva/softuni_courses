a8163dd7ba331b31774531473b6ac2fce4024f9c
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-29 00:15:50 +0300

    Add a solution

---
+++ csharp_basics/02.ConditionalStatements/Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,18 @@
+﻿string dramaName = Console.ReadLine();
+int episodeDuration = int.Parse(Console.ReadLine());
+int breakDuration = int.Parse(Console.ReadLine());
+
+double lunchDuration = breakDuration / 8d;
+double leisureDuration = breakDuration / 4d;
+double freeTime = breakDuration - lunchDuration - leisureDuration;
+
+double difference = Math.Abs(episodeDuration - freeTime);
+
+if (freeTime >= episodeDuration)
+{
+    Console.WriteLine($"You have enough time to watch {dramaName} and left with {Math.Ceiling(difference)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {dramaName}, you need {Math.Ceiling(difference)} more minutes.");
+}
