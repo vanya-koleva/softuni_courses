c380e493e59ebde3505f3d32096c6fdd3b80433d
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-30 01:21:09 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Lab/04.PersonalTitles/Program.cs
@@ -0,0 +1,12 @@
+﻿double age = double.Parse(Console.ReadLine());
+string gender = Console.ReadLine();
+
+switch (gender)
+{
+    case "m":
+        Console.WriteLine(age >= 16 ? "Mr." : "Master");
+        break;
+    case "f":
+        Console.WriteLine(age >= 16 ? "Ms." : "Miss");
+        break;
+}
