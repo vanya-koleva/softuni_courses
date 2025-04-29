dd870f4e0c496f069e64619628435a9eae2f1fa2
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-30 01:23:05 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Lab/08.CinemaTicket/Program.cs
@@ -0,0 +1,21 @@
+﻿string day = Console.ReadLine().Trim().ToLowerInvariant();
+
+switch (day)
+{
+    case "monday":
+    case "tuesday":
+    case "friday":
+        Console.WriteLine("12");
+        break;
+    case "wednesday":
+    case "thursday":
+        Console.WriteLine("14");
+        break;
+    case "saturday":
+    case "sunday":
+        Console.WriteLine("16");
+        break;
+    default:
+        Console.WriteLine("Invalid day.");
+        break;
+}
