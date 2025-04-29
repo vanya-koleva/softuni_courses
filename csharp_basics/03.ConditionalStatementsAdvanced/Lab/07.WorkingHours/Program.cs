b26b754e9738513d017ae2fe32c9dbe1feb8b8cf
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-04-30 01:22:44 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Lab/07.WorkingHours/Program.cs
@@ -0,0 +1,15 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+bool isWorkingDay = day is "Monday" or "Tuesday" or "Wednesday"
+                        or "Thursday" or "Friday" or "Saturday";
+bool isWorkingHour = hour is >= 10 and <= 18;
+
+if (isWorkingDay && isWorkingHour)
+{
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
