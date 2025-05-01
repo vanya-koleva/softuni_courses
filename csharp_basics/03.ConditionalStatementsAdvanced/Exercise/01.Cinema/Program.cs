7d442792316de5ed4721b86efd951b01e002ae82
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-01 14:01:57 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Exercise/01.Cinema/Program.cs
@@ -0,0 +1,26 @@
+﻿string projectionType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
+
+const double premierePrice = 12.0;
+const double normalPrice = 7.5;
+const double discountPrice = 5.0;
+
+double price = 0;
+
+switch (projectionType)
+{
+    case "Premiere":
+        price = premierePrice;
+        break;
+    case "Normal":
+        price = normalPrice;
+        break;
+    case "Discount":
+        price = discountPrice;
+        break;
+}
+
+double total = price * rows * columns;
+
+Console.WriteLine($"{total:F2} leva");
