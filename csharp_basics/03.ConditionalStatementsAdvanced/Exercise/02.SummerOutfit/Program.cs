7bda2130aa4bdfb19ce3715adf841b998765f948
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-01 14:02:25 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Exercise/02.SummerOutfit/Program.cs
@@ -0,0 +1,49 @@
+﻿double degrees = double.Parse(Console.ReadLine());
+string timeOfDay = Console.ReadLine();
+
+string outfit = "";
+string shoes = "";
+
+switch (timeOfDay)
+{
+    case "Morning":
+        if (degrees is >= 10 and <= 18)
+        {
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+        } 
+        else if (degrees is > 18 and <= 24)
+        {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } 
+        else if (degrees is >= 25)
+        {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        }
+        break;
+    case "Afternoon":
+        if (degrees is >= 10 and <= 18)
+        {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } 
+        else if (degrees is > 18 and <= 24)
+        {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        } 
+        else if (degrees is >= 25)
+        {
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+        }
+        break;
+    default:
+        outfit = "Shirt";
+        shoes = "Moccasins";
+        break;
+}
+
+Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
