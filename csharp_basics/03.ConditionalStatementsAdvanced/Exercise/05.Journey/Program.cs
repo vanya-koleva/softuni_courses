03547759c98e77b4276932a94a34efec9b438edd
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-02 14:26:00 +0300

    Add a solution

---
+++ csharp_basics/03.ConditionalStatementsAdvanced/Exercise/05.Journey/Program.cs
@@ -0,0 +1,54 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+string destination = "";
+string accomodation = "";
+double percent = 0.0;
+
+switch (season)
+{
+    case "summer":
+        accomodation = "Camp";
+        break;
+    default:
+        accomodation = "Hotel";
+        break;
+}
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    
+    if (season == "summer")
+    {
+        percent = 0.3;
+    }
+    else
+    {
+        percent = 0.7;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    
+    if (season == "summer")
+    {
+        percent = 0.4;
+    }
+    else
+    {
+        percent = 0.8;
+    }
+}
+else
+{
+    destination = "Europe";
+    percent = 0.9;
+    accomodation = "Hotel";
+}
+
+double price = budget * percent;
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{accomodation} - {price:F2}");
