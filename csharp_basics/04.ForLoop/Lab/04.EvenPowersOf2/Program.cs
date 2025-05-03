2db3c52f0c04e3570d231e2f10ce3ea17b6d456a
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-03 13:43:20 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Lab/04.EvenPowersOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.EvenPowersOf2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
