b42bd77071a0fb3f68b42609e3b87178b9d4e7d6
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-04 02:14:50 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Lab/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _09.LeftAndRightSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int leftSum = 0, rightSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            leftSum += number;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            rightSum += number;
+        }
+
+        if (leftSum == rightSum)
+        {
+            Console.WriteLine($"Yes, sum = {leftSum}");
+        }
+        else
+        {
+            Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+        }
+    }
+}
