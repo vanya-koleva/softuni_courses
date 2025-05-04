4968a101c26d997b9b37c83b4297207def19b439
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-05 01:32:58 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Exercise/06.Oscars/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int judgeCount = int.Parse(Console.ReadLine());
+
+            const double PointsNeeded = 1250.5;
+
+            double currentPoints = academyPoints;
+
+            for (int i = 0; i < judgeCount; i++)
+            {
+                string judgeName = Console.ReadLine();
+                double judgePoints = double.Parse(Console.ReadLine());
+
+                currentPoints += judgePoints * judgeName.Length / 2;
+
+                if (currentPoints > PointsNeeded)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {currentPoints:F1}!");
+                    return;
+                }
+            }
+
+            double pointsNeededDifference = PointsNeeded - currentPoints;
+
+            Console.WriteLine($"Sorry, {actorName} you need {pointsNeededDifference:F1} more!");
+        }
+    }
+}
