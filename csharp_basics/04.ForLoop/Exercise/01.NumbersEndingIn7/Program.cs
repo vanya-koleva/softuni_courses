21c280e6f49b03ac4481e2852fd0274e929625c1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-04 16:18:44 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Exercise/01.NumbersEndingIn7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.NumbersEndingIn7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 7; i <= 997; i ++)
+        {
+            if (i % 10 == 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
