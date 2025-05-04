252a6c24f0eb5b3ceaab5d45093a79f5bbf5928c
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-04 16:19:18 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.HalfSumElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int sum = 0;
+        int max = int.MinValue;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            
+            sum += num;
+
+            if (max < num)
+            {
+                max = num;
+            }
+        }
+        
+        sum -= max;
+
+        if (max == sum)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine($"Sum = {sum}");
+        }
+        else
+        {
+            Console.WriteLine("No");
+            Console.WriteLine($"Diff = {Math.Abs(sum - max)}");
+        }
+    }
+}
