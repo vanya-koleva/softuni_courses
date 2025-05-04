f56b9840051187cbb3efdb7fb89ad77a70435564
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-05 01:32:44 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Exercise/05.Salary/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _05.Salary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double salary = double.Parse(Console.ReadLine());
+
+        const int facebookFee = 150;
+        const int instagramFee = 100;
+        const int redditFee = 50;
+        
+        double currentSalary = salary;
+
+        for (int i = 1; i <= n; i++)
+        {
+            string tab = Console.ReadLine();
+
+            if (tab == "Facebook")
+            {
+                currentSalary -= facebookFee;
+            }
+            else if (tab == "Instagram")
+            {
+                currentSalary -= instagramFee;
+            }
+            else if (tab == "Reddit")
+            {
+                currentSalary -= redditFee;
+            }
+
+            if (currentSalary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+                return;
+            }
+        }
+        
+        Console.WriteLine((int)currentSalary);
+    }
+}
