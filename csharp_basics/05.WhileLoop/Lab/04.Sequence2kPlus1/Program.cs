ee50e64b4b8d2457026acbe1ae398ca71987181d
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-06 00:54:37 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/04.Sequence2kPlus1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.Sequence2kPlus1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int limit = int.Parse(Console.ReadLine());
+
+        int currentNumber = 1;
+
+        while (currentNumber <= limit)
+        {
+            Console.WriteLine(currentNumber);
+
+            currentNumber = currentNumber * 2 + 1;
+        }
+    }
+}
