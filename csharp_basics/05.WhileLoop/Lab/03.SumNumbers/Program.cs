09955d81fb272a376e71895394614a6e9fbfbdf1
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-05 13:37:35 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/03.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.SumNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int targetNum = int.Parse(Console.ReadLine());
+        
+        int sum = 0;
+
+        while (sum < targetNum) 
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            sum += number;
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
