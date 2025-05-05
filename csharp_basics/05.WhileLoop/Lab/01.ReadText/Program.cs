f559c5e3465dc3f1785ec9d1ca13a76b34058751
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-05 13:37:06 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/01.ReadText/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ReadText;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "Stop")
+            {
+                break;
+            }
+            
+            Console.WriteLine(input);
+        }
+    }
+}
