780f81f81224e4ef51b580b606049154c4874b0f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-06 00:55:57 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/08.Graduation/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _08.Graduation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+
+        const double passingGrade = 4.00;
+
+        int fails = 0;
+        double totalGrades = 0;
+        int currentClass = 1;
+
+        while (true)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade < passingGrade)
+            {
+                fails++;
+                if (fails > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {currentClass} grade");
+                    break;
+                }
+                
+                continue;
+            }
+            
+            totalGrades += grade;
+            currentClass++;
+
+            if (currentClass > 12)
+            {
+                double averageGrade = totalGrades / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:F2}");
+                break;
+            }
+        }
+    }
+}
