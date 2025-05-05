d1e3a7eb1d674a4b75276734e3e8b9fc13257dc4
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-05 13:15:32 +0300

    Add a solution

---
+++ csharp_basics/04.ForLoop/Exercise/07.TrekkingMania/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.TrekkingMania;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int groupsNum = int.Parse(Console.ReadLine());
+
+        int musalaClimbers = 0;
+        int montBlancClimbers = 0;
+        int kilimanjaroClimbers = 0;
+        int k2Climbers = 0;
+        int everestClimbers = 0;
+        int totalClimbers = 0;
+
+        for (int group = 0; group < groupsNum; group++)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            
+            if (persons <= 5) musalaClimbers += persons;
+            else if (persons <= 12) montBlancClimbers += persons;
+            else if (persons <= 25) kilimanjaroClimbers += persons;
+            else if (persons <= 40) k2Climbers += persons;
+            else everestClimbers += persons;
+            
+            totalClimbers += persons;
+        }
+        
+        Console.WriteLine($"{(double)musalaClimbers / totalClimbers * 100:F2}%");
+        Console.WriteLine($"{(double)montBlancClimbers / totalClimbers * 100:F2}%");
+        Console.WriteLine($"{(double)kilimanjaroClimbers / totalClimbers * 100:F2}%");
+        Console.WriteLine($"{(double)k2Climbers / totalClimbers * 100:F2}%");
+        Console.WriteLine($"{(double)everestClimbers / totalClimbers * 100:F2}%");
+    }
+}
