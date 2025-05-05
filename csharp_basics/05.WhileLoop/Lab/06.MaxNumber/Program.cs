00c4e305233f92f7511565cd5ebba37de60fb806
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-06 00:55:05 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/06.MaxNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.MaxNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+        
+        int maxNumber = int.MinValue;
+
+        while (command != "Stop")
+        {
+            int num = int.Parse(command);
+
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+            
+            command = Console.ReadLine();
+        }
+        
+        Console.WriteLine(maxNumber);
+    }
+}
