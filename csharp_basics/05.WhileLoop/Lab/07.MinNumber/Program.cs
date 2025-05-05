ee9fac24d7db29c6a936be4adbd011eb1ec8946f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-06 00:55:45 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/07.MinNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.MinNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int min = int.MaxValue;
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "Stop")
+            {
+                break;
+            }
+            
+            int number = int.Parse(command);
+
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        
+        Console.WriteLine(min);
+    }
+}
