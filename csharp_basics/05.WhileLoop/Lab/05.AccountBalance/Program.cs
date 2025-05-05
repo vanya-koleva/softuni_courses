71d7ecec4a2fd939525c090bb025f2c13f7b9a78
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-06 00:54:51 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Lab/05.AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.AccountBalance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+        
+        double total = 0;
+
+        while (command != "NoMoreMoney")
+        {
+            double currentSum = double.Parse(command);
+
+            if (currentSum < 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+            
+            Console.WriteLine($"Increase: {currentSum:F2}");
+            total += currentSum;
+            
+            command = Console.ReadLine();
+        }
+        
+        Console.WriteLine($"Total: {total:F2}");
+    }
+}
