2f1a12dd2554cd125619e5aaffb35db9758e765a
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-07 00:48:32 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Exercise/01.OldBooks/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.OldBooks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string searchedBook = Console.ReadLine();
+        int counter = 0;
+        
+        while (true)
+        {
+            string input = Console.ReadLine();
+            
+            if (input == searchedBook)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+                break;
+            }
+
+            if (input == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+                break;
+            }
+            
+            counter++;
+        }
+    }
+}
