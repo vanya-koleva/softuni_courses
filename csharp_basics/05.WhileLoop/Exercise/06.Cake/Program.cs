de4f1b728e9ca497f7cdbca5849f21cc9ff02f7d
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-08 12:56:41 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Exercise/06.Cake/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.Cake;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int pieces = int.Parse(Console.ReadLine()) * int.Parse(Console.ReadLine());
+
+        while (pieces > 0)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "STOP")
+            {
+                break;
+            }
+            
+            pieces -= int.Parse(command);
+        }
+
+        if (pieces <= 0)
+        {
+            Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+        }
+        else
+        {
+            Console.WriteLine($"{pieces} pieces are left.");
+        }
+    }
+}
