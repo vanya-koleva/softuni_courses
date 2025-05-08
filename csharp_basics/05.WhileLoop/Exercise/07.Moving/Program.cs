0078bfce9ee0093fea7ece5bec26f80ebc4e16b2
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-08 12:56:52 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Exercise/07.Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.Moving;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double width = double.Parse(Console.ReadLine());
+        double length = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        
+        double availableSpace = width * length * height;
+
+        while (availableSpace > 0)
+        {
+            var command = Console.ReadLine();
+
+            if (command == "Done")
+            {
+                break;
+            }
+            
+            availableSpace -= int.Parse(command);
+        }
+
+        if (availableSpace >= 0)
+        {
+            Console.WriteLine($"{availableSpace} Cubic meters left.");
+        }
+        else
+        {
+            Console.WriteLine($"No more free space! You need {Math.Abs(availableSpace)} Cubic meters more.");
+        }
+    }
+}
