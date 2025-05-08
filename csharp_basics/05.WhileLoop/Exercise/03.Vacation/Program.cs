3bfe45b1ec76ad7bde644953e9451ea28393ad68
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-08 12:55:44 +0300

    Add a solution

---
+++ csharp_basics/05.WhileLoop/Exercise/03.Vacation/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.Vacation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int maxSpendingDays = 5;
+        
+        double neededMoney = double.Parse(Console.ReadLine());
+        double availableMoney = double.Parse(Console.ReadLine());
+        
+        int daysCounter = 0;
+        int spendingDaysCounter = 0;
+
+        while (neededMoney > availableMoney)
+        {
+            string action = Console.ReadLine();
+            double sum = double.Parse(Console.ReadLine());
+            
+            daysCounter++;
+
+            if (action == "spend")
+            {
+                availableMoney -= sum;
+                availableMoney = Math.Max(availableMoney, 0);
+                spendingDaysCounter++;
+
+                if (spendingDaysCounter == maxSpendingDays)
+                {
+                    Console.WriteLine($"You can't save the money.");
+                    Console.WriteLine(daysCounter);
+                    break;
+                }
+            }
+            else
+            {
+                availableMoney += sum;
+                spendingDaysCounter = 0;
+            }
+        }
+
+        if (availableMoney >= neededMoney)
+        {
+            Console.WriteLine($"You saved the money for {daysCounter} days.");
+        }
+    }
+}
