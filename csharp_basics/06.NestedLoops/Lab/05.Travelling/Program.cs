4d02dc16ea13ade5da4ed2928de10784890d36dd
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-09 17:05:42 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Lab/05.Travelling/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.Travelling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string destination = Console.ReadLine();
+
+        while (destination != "End")
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double currentMoney = 0;
+
+            while (currentMoney < neededMoney)
+            {
+                currentMoney += double.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine($"Going to {destination}!");
+            destination = Console.ReadLine();
+        }
+    }
+}
