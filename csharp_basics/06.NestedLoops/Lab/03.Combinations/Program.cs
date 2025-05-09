aaed49eafeefeb3dab28a1b239428aa5bceb4e5f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-09 09:47:12 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Lab/03.Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.Combinations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        
+        int combinations = 0;
+
+        for (int i = 0; i <= number; i++)
+        {
+            for (int j = 0; j <= number; j++)
+            {
+                for (int k = 0; k <= number; k++)
+                {
+                    if (i + j + k == number)
+                    {
+                        combinations++;
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine(combinations);
+    }
+}
