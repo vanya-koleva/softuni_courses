80054fb2afa5aaacb6a32f1bff17fa1a24a0e87f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-09 17:05:52 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Lab/06.Building/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06.Building;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int floors = int.Parse(Console.ReadLine());
+        int rooms = int.Parse(Console.ReadLine());
+
+        string floorLetter = "";
+
+        for (int floor = floors; floor > 0; floor--)
+        {
+            if (floor == floors)
+            {
+                floorLetter = "L";
+            }
+            else if (floor % 2 == 0)
+            {
+                floorLetter = "O";
+            }
+            else
+            {
+                floorLetter = "A";
+            }
+
+            for (int room = 0; room < rooms; room++)
+            {
+                Console.Write($"{floorLetter}{floor}{room} ");
+            }
+            
+            Console.WriteLine("");
+        }
+    }
+}
