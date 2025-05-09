3fdc70588a3392f5cb01c815aca2e274641a7e79
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-09 17:05:32 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.SumOfTwoNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int magic = int.Parse(Console.ReadLine());
+        
+        int combinations = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                combinations++;
+
+                if (i + j == magic)
+                {
+                    Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magic})");
+                    return;
+                }
+            }
+        }
+        
+        Console.WriteLine($"{combinations} combinations - neither equals {magic}");
+    }
+}
