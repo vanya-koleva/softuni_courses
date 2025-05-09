ba8b8fe4eaab69df14235798aa6b019808409f30
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-09 17:13:45 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.NumberPyramid;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int current = 1;
+        bool isDone = false;
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(current + " ");
+                current++;
+                if (current > n)
+                {
+                    isDone = true;
+                    break;
+                }
+            }
+            
+            Console.WriteLine();
+            if (isDone)
+            {
+                break;
+            }
+        }
+    }
+}
