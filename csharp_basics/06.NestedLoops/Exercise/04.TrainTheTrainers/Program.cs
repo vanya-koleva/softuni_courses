a02ae55d9a9933747c133ce47bf6f056f8ebb9ac
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-10 16:42:25 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04.TrainTheTrainers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int juryNumber = int.Parse(Console.ReadLine());
+        
+        int presentations = 0;
+        double totalAverage = 0;
+
+        while (true)
+        {
+            string presentation = Console.ReadLine();
+
+            if (presentation == "Finish")
+            {
+                break;
+            }
+
+            double totalGrades = 0;
+
+            for (int i = 0; i < juryNumber; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                totalGrades += grade;
+            }
+            
+            double average = totalGrades / juryNumber;
+            totalAverage += average;
+            presentations++;
+            Console.WriteLine($"{presentation} - {average:F2}.");
+        }
+        
+        Console.WriteLine($"Student's final assessment is {totalAverage / presentations:F2}.");
+    }
+}
