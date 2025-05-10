4b864464c70693c2456ed6672b7002600eeb608a
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-10 17:13:21 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Exercise/05.SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.SpecialNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int num = 1111; num <= 9999; num++)
+        {
+            string numAsString = num.ToString();
+            bool isSpecial = true;
+
+            for (int i = 0; i < numAsString.Length; i++)
+            {
+                int digit = int.Parse(numAsString[i].ToString());
+                if (digit == 0 || n % digit != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+            }
+
+            if (isSpecial)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
