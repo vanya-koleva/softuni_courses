f50512b9ad09aee10d9746659bfdd084200bb8ae
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-10 16:42:04 +0300

    Add a solution

---
+++ csharp_basics/06.NestedLoops/Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.EqualSumsEvenOddPosition;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            int oddSum = 0;
+            int evenSum = 0;
+            string numberAsStr = i.ToString();
+
+            for (int j = 0; j < numberAsStr.Length; j++)
+            {
+                int digit = int.Parse(numberAsStr[j].ToString());
+
+                if (j % 2 == 0)
+                {
+                    evenSum += digit;
+                }
+                else
+                {
+                    oddSum += digit;
+                }
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
