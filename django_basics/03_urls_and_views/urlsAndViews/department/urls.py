2ad8e0ae4d06705efcfb27d4b92e76d12d4eaa4b
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-05-23 06:40:08 +0300

    Add a solution

--- django_basics/03_urls_and_views/urlsAndViews/department/urls.py
+++ django_basics/03_urls_and_views/urlsAndViews/department/urls.py
@@ -5,6 +5,8 @@ from department import views
 urlpatterns = [
     path('', views.index),
     path('<int:pk>/', views.int_param_view),
+    path('<uuid:id>/', views.uuid_param_view),
     path('<slug:slug>/',views.slug_param_view),
     path('<name>/', views.str_param_view),
+    path('<path:path_to_file>/', views.file_path_param_view),
 ]
--- django_basics/03_urls_and_views/urlsAndViews/department/views.py
+++ django_basics/03_urls_and_views/urlsAndViews/department/views.py
@@ -7,12 +7,23 @@ from department.models import Department
 def index(request):
     return HttpResponse("<h1>Departments Home Page</h1>")
 
+
 def int_param_view(request, pk):
     return HttpResponse(f"<h1>You are looking at department number {pk}</h1>")
+
 
 def str_param_view(request, name):
     return HttpResponse(f"<h1>Department Name: {name}</h1>")
 
+
 def slug_param_view(request, slug):
     department = Department.objects.filter(slug=slug).first()
     return HttpResponse(department)
+
+
+def file_path_param_view(request, path_to_file):
+    return HttpResponse(f"<h1>The file is located at: {path_to_file}</h1>")
+
+
+def uuid_param_view(request, id):
+    return HttpResponse(f"<h1>The UUID is: {id}</h1>")
