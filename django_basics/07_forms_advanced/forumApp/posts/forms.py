64ab2dc56deb6c7bdde6dc67a26f3cbf755f94a0
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-06-14 18:00:24 +0300

    Add a solution

---
+++ django_basics/07_forms_advanced/forumApp/posts/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+
+from posts.models import Post
+
+
+class PostBaseForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+        widgets = {
+            'language': forms.RadioSelect(
+                attrs={'class': 'radio-select'},
+            )
+        }
+
+        error_messages = {
+            'author': {
+                'max_length': "Hey, that's too much",
+            }
+        }
+
+
+class PostCreateForm(PostBaseForm):
+    pass
+
+
+class PostEditForm(PostBaseForm):
+    pass
+
+
+class PostDeleteForm(PostBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].disabled = True
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label='',
+        required=False,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Search...'}
+        )
+    )
+
