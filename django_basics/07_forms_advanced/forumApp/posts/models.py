b17ec4ce7817d975ca9fefece9363a8a60c79c74
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-06-18 22:09:18 +0300

    Add a solution

--- django_basics/07_forms_advanced/forumApp/posts/models.py
+++ django_basics/07_forms_advanced/forumApp/posts/models.py
@@ -34,3 +34,20 @@ class Post(models.Model):
         default=LanguageChoices.OTHER,
     )
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+
+    author = models.CharField(
+        max_length=50,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
