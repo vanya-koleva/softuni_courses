acdbc320a23c0d398341849cc033f99289cb5474
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-07-08 06:26:33 +0300

    Add a solution

--- django_basics/10_exam_preparation/musicApp/profiles/urls.py
+++ django_basics/10_exam_preparation/musicApp/profiles/urls.py
@@ -4,4 +4,5 @@ from profiles import views
 
 urlpatterns = [
     path('details/', views.ProfileDetailView.as_view(), name='profile-details'),
+    path('delete/', views.ProfileDeleteView.as_view(), name='profile-delete')
 ]
