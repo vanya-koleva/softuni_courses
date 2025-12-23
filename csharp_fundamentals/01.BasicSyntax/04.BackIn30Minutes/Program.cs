4b04791354ee53400311d6451d38a724c31cb7dd
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-23 15:30:32 +0200

    Add a solution

---
+++ csharp_fundamentals/01.BasicSyntax/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+const int minutesPerHour = 60;
+const int minutesToAdd = 30;
+
+minutes += minutesToAdd;
+
+if (minutes >= minutesPerHour)
+{
+    if (hours == 23)
+    {
+        hours = 0;
+    }
+    else
+    {
+        hours++;
+    }
+
+    minutes -= minutesPerHour;
+}    
+
+Console.WriteLine($"{hours}:{minutes:d2}");
