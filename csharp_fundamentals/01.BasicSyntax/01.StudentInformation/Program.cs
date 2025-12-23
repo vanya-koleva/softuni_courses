c61eb57484075b80cfd068a61ac04f784cf6317d
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-23 15:08:46 +0200

    Add a solution

---
+++ csharp_fundamentals/01.BasicSyntax/01.StudentInformation/Program.cs
@@ -0,0 +1,5 @@
+﻿string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+double avgGrade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {avgGrade:F2}");
