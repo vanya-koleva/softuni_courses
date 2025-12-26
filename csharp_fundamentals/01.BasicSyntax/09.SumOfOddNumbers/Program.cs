5f82fd87483508b74e11cce747f8dc6bc390202f
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-26 22:35:05 +0200

    Add a solution

---
+++ csharp_fundamentals/01.BasicSyntax/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+int oddNum = 1;
+int sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(oddNum);
+    sum += oddNum;
+    oddNum += 2;
+}
+
+Console.WriteLine($"Sum: {sum}");
