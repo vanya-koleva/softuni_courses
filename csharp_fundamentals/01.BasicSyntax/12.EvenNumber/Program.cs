f58ed2a5973a503dab5d37959cf949eed94ec0ae
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-26 22:37:08 +0200

    Add a solution

---
+++ csharp_fundamentals/01.BasicSyntax/12.EvenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    int n = int.Parse(Console.ReadLine());
+
+    if (n % 2 == 0)
+    {
+        Console.Write($"The number is: {Math.Abs(n)}");
+        break;
+    }
+    
+    Console.WriteLine("Please write an even number.");
+}
