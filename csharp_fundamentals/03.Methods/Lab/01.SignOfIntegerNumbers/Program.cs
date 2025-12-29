969cbb09f7ce7a1a43fc39f8ea82a1f0a7b01087
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-29 21:21:18 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(CheckSign(number));
+
+static string CheckSign(int num)
+{
+    string result;
+
+    if (num == 0)
+    {
+        result = $"The number {num} is zero. ";
+    }
+    else
+    {
+        result = $"The number {num} is {(num > 0 ? "positive" : "negative")}.";
+    }
+    
+    return result;
+}
