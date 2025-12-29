6fdd63035f12f9dce43803885476c9af655a69da
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-29 19:10:31 +0200

    Add a solution

---
+++ csharp_fundamentals/02.DataTypesAndVariables/Lab/08.TownInfo/Program.cs
@@ -0,0 +1,7 @@
+﻿string townName = Console.ReadLine();
+int population = int.Parse(Console.ReadLine());
+int area =  int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+
+// Console.WriteLine($"Town {Console.ReadLine()} has population of {Console.ReadLine()} and area {Console.ReadLine()} square km. ");
