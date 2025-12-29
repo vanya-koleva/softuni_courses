901990abc4b107f2844aa0cc1d6ca08fcc713224
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-29 19:09:59 +0200

    Add a solution

---
+++ csharp_fundamentals/02.DataTypesAndVariables/Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<int> specialSums = new() { 5, 7, 11 };
+
+for (int i = 1; i <= n; i++)
+{
+    bool isSpecialNum = false;
+    int sumOfDigits = 0;
+    int number = i;
+
+    while (number > 0)
+    {
+        sumOfDigits += number % 10;
+        number /= 10;
+    }
+
+    if (specialSums.Contains(sumOfDigits))
+    {
+        isSpecialNum = true;
+    }
+    
+    Console.WriteLine($"{i} -> {isSpecialNum}");
+}
