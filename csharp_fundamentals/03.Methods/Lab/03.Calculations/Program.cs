330be971ae49cc551ab626496d59ad0eabc0e477
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-31 14:52:24 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/03.Calculations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+string command = Console.ReadLine();
+int numberOne = int.Parse(Console.ReadLine());
+int numberTwo = int.Parse(Console.ReadLine());
+
+switch (command)
+{
+    case "add":
+        Add(numberOne, numberTwo);
+        break;
+    case "subtract":
+        Subtract(numberOne, numberTwo);
+        break;
+    case "multiply":
+        Multiply(numberOne, numberTwo);
+        break;
+    case "divide":
+        Divide(numberOne, numberTwo);
+        break;
+    default:
+        break;
+}
+
+static void Add(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+
+static void Subtract(int a, int b)
+{
+    Console.WriteLine(a - b);
+}
+
+static void Multiply(int a, int b)
+{
+    Console.WriteLine(a * b);
+}
+
+static void Divide(int a, int b)
+{
+    Console.WriteLine(a / b);
+}
