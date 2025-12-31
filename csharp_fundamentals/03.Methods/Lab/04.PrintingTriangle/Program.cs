7e3eb8ceb370549e5c31c89e214c542fe327470b
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-12-31 14:53:18 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿int size = int.Parse( Console.ReadLine());
+
+PrintTriangle(size);
+
+static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++) 
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintFirstHalf(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        PrintLine(1, i);
+    }
+}
+
+static void PrintSecondHalf(int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        PrintLine(1, i);
+    }
+}
+
+static void PrintTriangle(int size) 
+{
+    PrintFirstHalf(size);
+    PrintSecondHalf(size);
+}
