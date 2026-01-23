1b819c9ee5b861e6a29222ec4f2bc2a512a634ad
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-01-23 17:37:21 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/07.RepeatString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _07.RepeatString;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        int count = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(RepeatString(str, count));
+
+        static string RepeatString(string text, int n)
+        {
+            return string.Concat(Enumerable.Repeat(text, n));
+        }
+    }
+}
