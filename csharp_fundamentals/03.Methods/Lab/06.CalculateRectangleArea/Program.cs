6259e9164bbbabcc9bbb0ffc300fb3a44ba00a89
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-01-23 17:37:04 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.CalculateRectangleArea;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        double area = GetArea(width, height);
+        Console.WriteLine(area);
+        
+        static double GetArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
