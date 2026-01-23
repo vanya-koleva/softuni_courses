10b45676e3f76ed60737e6576a7b0cbd95faa543
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-01-23 17:36:53 +0200

    Add a solution

---
+++ csharp_fundamentals/03.Methods/Lab/05.Orders/Program.cs
@@ -0,0 +1,23 @@
+﻿string productName = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+static string GetPrice(string product, int qty)
+{
+    var prices = new Dictionary<string, double>
+    {
+        ["coffee"] = 1.50,
+        ["water"] = 1.00,
+        ["coke"] = 1.40,
+        ["snacks"] = 2.00
+    };
+    
+    double totalPrice = prices[product]  * qty;
+    return totalPrice.ToString("F2");
+}
+
+static void PrintPrice(string product, int qty)
+{
+    Console.WriteLine(GetPrice(product, qty));
+}
+
+PrintPrice(productName,  quantity);
