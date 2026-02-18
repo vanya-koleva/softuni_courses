796d6c3891fb1f10d9e89e0756e45702245115d8
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-18 15:42:39 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.SumEvenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        
+        int totalEven =  numbers.Sum(n => n % 2 == 0 ? n : 0);
+        
+        Console.WriteLine(totalEven);
+    }
+}
