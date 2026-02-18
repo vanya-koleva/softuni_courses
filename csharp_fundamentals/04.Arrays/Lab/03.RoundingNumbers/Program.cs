2df1529010da49d64eb2a8c7a3d2661f8054f4fa
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-18 15:41:42 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.RoundingNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] nums =  Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToArray();
+        
+        int[] newNums = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            newNums[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+            Console.WriteLine($"{nums[i]} => {newNums[i]}");
+        }
+    }
+}
