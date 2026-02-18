0c81f3a722483733847868f1abf6acd22aa0e691
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-18 15:42:14 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _04.ReverseArrayOfStrings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] words = Console.ReadLine().Split(" ");
+        
+        Console.WriteLine(string.Join(" ", words.Reverse()));
+    }
+}
