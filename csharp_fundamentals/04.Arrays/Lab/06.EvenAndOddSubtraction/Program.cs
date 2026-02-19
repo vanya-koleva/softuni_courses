317c595f36df6d1f0d610d11586900094be95e25
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-19 14:12:36 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Lab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06.EvenAndOddSubtraction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        
+        int evenSum = nums.Sum(n => n % 2 == 0 ? n : 0);
+        int oddSum = nums.Sum(n => n % 2 != 0 ? n : 0);
+        int difference = evenSum - oddSum;
+        
+        Console.WriteLine(difference);
+    }
+}
