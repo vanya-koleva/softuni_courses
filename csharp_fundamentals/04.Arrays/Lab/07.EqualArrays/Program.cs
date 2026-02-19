1a6e2d5de3b7fbda5c673a3131f29db4c1be275c
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-19 14:12:47 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+
+namespace _07.EqualArrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] nums2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if (nums1[i] != nums2[i])
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                return;
+            }
+        }
+        
+        Console.WriteLine($"Arrays are identical. Sum: {nums1.Sum()}");
+    }
+}
