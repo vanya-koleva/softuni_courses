94d9299a56be538f2d3245b19e35d7a3691ffc54
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-20 17:20:34 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.CommonElements;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arr1 = Console.ReadLine().Split(' ');
+        string[] arr2 = Console.ReadLine().Split(' ');
+
+        string common = "";
+
+        foreach (var el in arr2)
+        {
+            if (arr1.Contains(el))
+            {
+                common += el + " ";
+            }
+        }
+        
+        Console.WriteLine(common);
+    }
+}
