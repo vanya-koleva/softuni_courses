b9039c1810ba59a82efc41715eaa40f9e90177c5
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-20 17:20:51 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.Zig_ZagArrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numLines = int.Parse(Console.ReadLine());
+        int[] arr1 = new int[numLines];
+        int[] arr2 = new int[numLines];
+
+        for (int i = 0; i < numLines; i++)
+        {
+            int[] currentLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            if ((i + 1)% 2 != 0)
+            {
+                arr1[i] = currentLine[0];
+                arr2[i] = currentLine[1];
+            }
+            else
+            {
+                arr1[i] = currentLine[1];
+                arr2[i] = currentLine[0];
+            }
+        }
+        
+        Console.WriteLine(string.Join(" ", arr1));
+        Console.WriteLine(string.Join(" ", arr2));
+    }
+}
