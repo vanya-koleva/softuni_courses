fef32b8358c6fbe8769ef1b497aa40d1984b474a
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-20 17:20:14 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Exercise/01.Train/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Train;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfWagons  = int.Parse(Console.ReadLine());
+        
+        int[] peopleInWagon = new int[numberOfWagons];
+        int total = 0;
+
+        for (int i = 0; i < numberOfWagons; i++)
+        {
+            peopleInWagon[i] = int.Parse(Console.ReadLine());
+            total += peopleInWagon[i];
+        }
+        
+        Console.WriteLine(string.Join(" ", peopleInWagon));
+        Console.WriteLine(total);
+    }
+}
