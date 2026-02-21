762d8e72b89b5e5c924f42edd7ef4e50d0264069
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-02-21 16:28:35 +0200

    Add a solution

---
+++ csharp_fundamentals/04.Arrays/Exercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.ArrayRotation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int rotations = int.Parse(Console.ReadLine());
+
+        rotations %= arr.Length;
+        
+        int[] rotated = arr.Skip(rotations).Concat(arr.Take(rotations)).ToArray();
+        
+        Console.WriteLine(string.Join(" ", rotated));
+    }
+}
